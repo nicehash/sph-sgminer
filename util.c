7bb2c424725393280f5aaa3c6d04af5b614f4ffd
elbandi <bandispam@gmail.com>
2014-04-26 01:17:14 +0200

    Some tweaks for extranonce

--- util.c
+++ util.c
@@ -1681,12 +1681,10 @@ static bool parse_extranonce(struct pool *pool, json_t *val)
 
 	nonce1 = json_array_string(val, 0);
 	if (!nonce1) {
-//		applog(LOG_INFO, "Failed to get nonce1 in ");
 		return false;
 	}
 	n2size = json_integer_value(json_array_get(val, 1));
 	if (!n2size) {
-//		applog(LOG_INFO, "Failed to get n2size in ");
 		free(nonce1);
 		return false;
 	}
@@ -1702,7 +1700,7 @@ static bool parse_extranonce(struct pool *pool, json_t *val)
 	pool->n2size = n2size;
 	cg_wunlock(&pool->data_lock);
 
-	applog(LOG_NOTICE, "%s coin change requested", pool->poolname);
+	applog(LOG_NOTICE, "%s extranonce change requested", pool->poolname);
 
 	return true;
 }
@@ -1876,7 +1874,7 @@ bool subscribe_extranonce(struct pool *pool)
 	if (!stratum_send(pool, s, strlen(s)))
 		return ret;
 
-	/* Parse all data in the queue and anything left should be auth */
+	/* Parse all data in the queue and anything left should be the response */
 	while (42) {
 		if (!socket_full(pool, DEFAULT_SOCKWAIT / 30)) {
 			applog(LOG_DEBUG, "Timed out waiting for response extranonce.subscribe");
